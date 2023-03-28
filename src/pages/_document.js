d635062be83b170e2694d4c0683ebe78b2cc1c40
Yan Chummar <yanpooppada@gmail.com>
2023-03-29 04:45:36 +0530

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,300,400&display=swap" rel="stylesheet" />
+
+        <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-KWKX5Y1V0G" />
+        <Script
+          id="google-analytics"
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-KWKX5Y1V0G');
+            `
+          }} />
       </Head>
       <body>
         <Main />
